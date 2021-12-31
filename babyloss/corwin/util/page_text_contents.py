ac280526d11a0e01a5dadb002df085d4742d9f4a
Hunter Myers <myers.hunter@gmail.com>
2021-12-31 15:06:47 -0600

    lots of updates

--- babyloss/corwin/util/page_text_contents.py
+++ babyloss/corwin/util/page_text_contents.py
@@ -2,24 +2,35 @@ import re
 from django.utils.safestring import mark_safe
 
 SOURCE_KEYWORD = 'source'
-SOURCE_START_DELIM = '['
-SOURCE_END_DELIM = ']'
+CODE_START_DELIM = '['
+CODE_END_DELIM = ']'
 LINK_MAX_CHARS = 30
+IMAGE_KEYWORD = 'image'
+CAPTION_KEYWORD = 'caption'
 
 
-def create_page_text_contents(page_items):
+def create_page_contents(page_items):
 
     # keep track of sources referenced in the text
     source_pattern = re.compile('(\\[source http.*?\\])')
     source_dict = {}
     source_num = 0
 
+    # look for images in the text
+    image_pattern = re.compile('(\\[image \\w+\\.\\w+\\])')
+
+    # look for image captions in the text
+    caption_pattern = re.compile('\\[caption \".+\"\\]')
+
     # iterate through the text for each text block
     for item in page_items:
         source_matches = source_pattern.findall(item.summary_text)
+        image_matches = image_pattern.findall(item.summary_text)
+        caption_matches = caption_pattern.findall(item.summary_text)
 
+        # create dict of source patterns
         for source_match in source_matches:
-            source_match_spl = source_match.replace(SOURCE_START_DELIM, '').replace(SOURCE_END_DELIM,'').split(' ')
+            source_match_spl = source_match.replace(CODE_START_DELIM, '').replace(CODE_END_DELIM, '').split(' ')
 
             # if this matches our source pattern, add it to the source dict
             if len(source_match_spl) == 2 and source_match_spl[0] == SOURCE_KEYWORD:
@@ -29,12 +40,27 @@ def create_page_text_contents(page_items):
                     source_num += 1
                     source_dict[source_match] = Hyperlink(source_num=source_num, url=url)
 
+        # replace image patterns with image HTML
+        for image_match in image_matches:
+            image_file = image_match.split(' ')[1].replace(CODE_END_DELIM,'')
+            image_html = '<img style="width: 100%" class="rounded" src="static/corwin/images/{0}" class="img-fluid">'.format(image_file)
+            item.summary_text = mark_safe(item.summary_text.replace(image_match, image_html))
+
+        # replace captions with caption text
+        for caption_match in caption_matches:
+            caption_text = caption_match.replace(CAPTION_KEYWORD, '').replace(CODE_START_DELIM, '').replace(CODE_END_DELIM, '').replace('"','')
+            caption_html = '<div class="caption-text">{0}</div>'.format(caption_text)
+            item.summary_text = mark_safe(item.summary_text.replace(caption_match, caption_html))
+
     # make another pass through the items and replace the source patterns with the HTMl we want
     for item in page_items:
         for source_key in source_dict:
             new_text = item.summary_text.replace(source_key, '<sup><a href="{0}" target="_blank">{1}</a></sup>'.format(
                 source_dict[source_key].url, source_dict[source_key].source_num))
             item.summary_text = mark_safe(new_text)
+
+        # mark safe all html
+        item.summary_text = mark_safe(item.summary_text)
 
     source_list = sorted([item for item in source_dict.values()], key=lambda x: x.source_num)
 
--- babyloss/corwin/views.py
+++ babyloss/corwin/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 from .models import PageTopic
-from corwin.util.page_text_contents import create_page_text_contents
+from corwin.util.page_text_contents import create_page_contents
 
 
 def index(request):
 
     page_items = PageTopic.objects.filter(page__page_name='home').order_by('page_order')
 
-    context = create_page_text_contents(page_items)
+    context = create_page_contents(page_items)
 
     return render(request, 'corwin/index.html', context)
 
@@ -16,7 +16,7 @@ def babyloss(request):
 
     page_items = PageTopic.objects.filter(page__page_name='babyloss').order_by('page_order')
 
-    context = create_page_text_contents(page_items)
+    context = create_page_contents(page_items)
 
     return render(request, 'corwin/babyloss.html', context)
 
@@ -25,7 +25,7 @@ def resources_expecting(request):
 
     page_items = PageTopic.objects.filter(page__page_name='resources_expecting').order_by('page_order')
 
-    context = create_page_text_contents(page_items)
+    context = create_page_contents(page_items)
 
     return render(request, 'corwin/resources_expecting.html', context)
 
@@ -34,7 +34,7 @@ def resources_loss(request):
 
     page_items = PageTopic.objects.filter(page__page_name='resources_loss').order_by('page_order')
 
-    context = create_page_text_contents(page_items)
+    context = create_page_contents(page_items)
 
     return render(request, 'corwin/resources_loss.html', context)
 
@@ -43,7 +43,7 @@ def resources_doulas(request):
 
     page_items = PageTopic.objects.filter(page__page_name='resources_doulas').order_by('page_order')
 
-    context = create_page_text_contents(page_items)
+    context = create_page_contents(page_items)
 
     return render(request, 'corwin/resources_doulas.html', context)
 
@@ -52,7 +52,7 @@ def resources_providers(request):
 
     page_items = PageTopic.objects.filter(page__page_name='resources_providers').order_by('page_order')
 
-    context = create_page_text_contents(page_items)
+    context = create_page_contents(page_items)
 
     return render(request, 'corwin/resources_providers.html', context)
 
@@ -61,7 +61,7 @@ def resources_loved(request):
 
     page_items = PageTopic.objects.filter(page__page_name='resources_loved').order_by('page_order')
 
-    context = create_page_text_contents(page_items)
+    context = create_page_contents(page_items)
 
     return render(request, 'corwin/resources_loved.html', context)
 
@@ -70,7 +70,7 @@ def resources_employers(request):
 
     page_items = PageTopic.objects.filter(page__page_name='resources_employers').order_by('page_order')
 
-    context = create_page_text_contents(page_items)
+    context = create_page_contents(page_items)
 
     return render(request, 'corwin/resources_employers.html', context)
 
@@ -79,7 +79,7 @@ def corwin_story(request):
 
     page_items = PageTopic.objects.filter(page__page_name='corwin_story').order_by('page_order')
 
-    context = create_page_text_contents(page_items)
+    context = create_page_contents(page_items)
 
     return render(request, 'corwin/corwin_story.html', context)
 
@@ -88,6 +88,6 @@ def about(request):
 
     page_items = PageTopic.objects.filter(page__page_name='about').order_by('page_order')
 
-    context = create_page_text_contents(page_items)
+    context = create_page_contents(page_items)
 
     return render(request, 'corwin/about.html', context)
