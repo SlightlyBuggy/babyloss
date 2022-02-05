b271f6af268990695c3b36c5c9a9d0c4b3b9dbc1
Hunter Myers <myers.hunter@gmail.com>
2022-02-05 13:39:33 -0600

    styling updates, added stub tests

---
+++ babyloss/corwin/util/test_page_text_contents.py
@@ -0,0 +1,27 @@
+from django.test import TestCase
+from util import page_text_contents
+from models import PageTopic
+
+
+class PageContentsTestCases(TestCase):
+
+    def setUp(self):
+        # TODO: create test objects with each type of keyword
+        pass
+
+    # TODO: test handling of each type of keyword individually then as a group
+    def testImageKeyword(self):
+        pass
+
+    def testCaptionKeyword(self):
+        pass
+
+    def testLinkKeyword(self):
+        pass
+
+    def testSourceList(self):
+        pass
+
+    def testPageTextContents(self):
+        pass
+
